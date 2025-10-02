329b6fab06d2878488bb98316ecdfb58728167df
Family <mh5212357@gmail.com>
2025-10-01 20:32:50 -0400

    I updated this project

--- src/main/java/com/pluralsight/CellPhonePt3.java
+++ src/main/java/com/pluralsight/CellPhonePt3.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhonePt3 {
+    public static void main(String[] args) {
+
+        // Create a scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the serial number?");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("What model is the phone");
+        String model = scanner.nextLine();
+        System.out.print("Who is the carrier?");
+        String carrier = scanner.nextLine();
+        System.out.print("What is the phone number?");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Who is the owner of the phone?");
+        String owner = scanner.nextLine();
+
+        // create object from class
+        CellPhonePt3_Info cellPhone3 = new CellPhonePt3_Info(serialNumber, model,carrier,phoneNumber,owner);
+        cellPhone3.setSerialNumber(serialNumber);
+        cellPhone3.setModel(model);
+        cellPhone3.setCarrier(carrier);
+        cellPhone3.setPhoneNumber(phoneNumber);
+        cellPhone3.setOwner(owner);
+
+        scanner.close();
+
+        display(cellPhone3);
+        cellPhone3.dial(cellPhone3);
+    }
+
+    public static void display(CellPhonePt3_Info phoneInfo){
+
+        // Getter part below of cellInfo
+        System.out.println("Serial Number is: " + phoneInfo.getSerialNumber());
+        System.out.println("Mobile model is: " + phoneInfo.getModel());
+        System.out.println("Cell phone carrier is: " + phoneInfo.getCarrier());
+        System.out.println("Phone number is: " + phoneInfo.getPhoneNumber());
+        System.out.println("Phone owner is: " + phoneInfo.getOwner());
+
+    }
 }
--- src/main/java/com/pluralsight/CellPhonePt3_Info.java
+++ src/main/java/com/pluralsight/CellPhonePt3_Info.java
@@ -1,4 +1,89 @@
 package com.pluralsight;
 
 public class CellPhonePt3_Info {
+    // Attribute always private
+        private int serialNumber;
+        private String model;
+        private String carrier;
+        private String phoneNumber;
+        private String owner;
+//============================================
+
+        public CellPhonePt3_Info() {
+            serialNumber = 0;
+            model = "";
+            carrier = "";
+            phoneNumber = "";
+            owner = "";
+        }
+// Constructor
+    public CellPhonePt3_Info(String model, int serialNumber, String carrier, String phoneNumber, String owner) {
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+    // Instance create for with parameter and without parameter
+    public CellPhonePt3_Info(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+
+    }
+    //Actions
+    public void dial(String number){
+        System.out.println( owner + "'s phone is calling " + number);
+    }
+    public void dial( CellPhonePt3_Info callInfo){
+        System.out.println( owner + "'s phone is calling " + callInfo.getPhoneNumber());
+    }
+    //Overloading
+
+    // color_String
+    public void color(String color){
+        System.out.println( "Phone color: " + color);
+
+    }
 }
+
+
+
+
